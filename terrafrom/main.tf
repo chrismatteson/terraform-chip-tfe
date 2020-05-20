ca824f10ed5558582f006c9d9400064d21049f7f
Chris Matteson <chris.matteson@gmail.com>
2020-05-20 10:56:47 -0700

    update to use loadbalancer name for certificate

--- terrafrom/main.tf
+++ terrafrom/main.tf
@@ -32,11 +32,11 @@ resource "tls_private_key" "aws_ssh_key" {
 
 resource "aws_key_pair" "aws_ssh_key" {
   key_name   = "${random_id.project_name.hex}-ssh-key"
-  public_key = "${tls_private_key.aws_ssh_key.public_key_openssh}"
+  public_key = tls_private_key.aws_ssh_key.public_key_openssh
 }
 
 resource "local_file" "private_key" {
-    content     = "${tls_private_key.aws_ssh_key.private_key_pem}"
+    content     = tls_private_key.aws_ssh_key.private_key_pem
     filename = "${path.module}/private.pem"
 }
 
@@ -45,7 +45,7 @@ resource "tls_self_signed_cert" "example" {
   private_key_pem = tls_private_key.aws_ssh_key.private_key_pem
 
   subject {
-    common_name  = "example.com"
+    common_name  = module.tfe.tfe_alb_dns_name
     organization = "ACME Examples, Inc"
   }
 
@@ -59,21 +59,22 @@ resource "tls_self_signed_cert" "example" {
 }
 
 resource "aws_acm_certificate" "cert" {
-  private_key      = "${tls_private_key.aws_ssh_key.private_key_pem}"
-  certificate_body = "${tls_self_signed_cert.example.cert_pem}"
+  private_key      = tls_private_key.aws_ssh_key.private_key_pem
+  certificate_body = tls_self_signed_cert.example.cert_pem
 }
 
 module "tfe" {
   source = "github.com/hashicorp/terraform-chip-tfe-is-terraform-aws-ptfe-v4-quick-install"
 
   friendly_name_prefix       = "tfe"
-  tfe_hostname               = "tfe.example.com"
+  tfe_hostname               = module.tfe.tfe_alb_dns_name
   tfe_license_file_path      = "terraform-chip.rli"
   vpc_id                     = module.vpc.vpc_id
   alb_subnet_ids             = module.vpc.public_subnets
   ec2_subnet_ids             = module.vpc.private_subnets
   rds_subnet_ids             = module.vpc.database_subnets
-  tls_certificate_arn        = aws_acm_certificate.cert.arn
+  tls_certificate_arn        = aws_acm_certificate.cert.id
+  tfe_initial_admin_pw       = "SomethingSecure!"
 }
 
 output "tfe_url" {
